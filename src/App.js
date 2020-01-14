e0a901c4dc38700f611ae2bb959c8ce72adb9e6f
Amit <PT@Amit-MacBook-Pro.local>
2020-01-14 22:58:46 +0530

    second commit

--- src/App.js
+++ src/App.js
@@ -16,7 +16,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn Python and react
+          Learn Python only
         </a>
       </header>
     </div>
